7b8db09bf63ef3af393b7d549c86c3be64120101
HyunSeungBeom <ghfk100@naver.com>
2024-12-12 19:20:49 +0900

    fix: css 수정

--- src/app/components/Skill.tsx
+++ src/app/components/Skill.tsx
@@ -34,7 +34,10 @@ const SkillSection = ({
         </div>
         <div className="w-full grid grid-cols-2 md:grid-cols-3 gap-4">
           {skills.map((skill, index) => (
-            <div key={index} className="bg-gray-500 p-4 rounded-lg shadow-md">
+            <div
+              key={index}
+              className="bg-gray-500 px-4 py-2 rounded-lg shadow-md"
+            >
               {skill}
             </div>
           ))}
