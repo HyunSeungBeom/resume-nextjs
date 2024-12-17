3edc891ee758491ddc609780ad81a4c9143b2066
HyunSeungBeom <ghfk100@naver.com>
2024-12-17 17:55:23 +0900

    image 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,16 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: 'seungbeom-resume',
+  description: '문제를 해결하며 성장을 즐기는 개발자, 현승범의 이력서입니다.',
+  openGraph: {
+    images: [
+      {
+        url: '/og-image.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -23,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="kr">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
