aba068758c05926173a25c18010b6e72786160cd
HyunSeungBeom <ghfk100@naver.com>
2024-12-18 16:24:49 +0900

    chore: Link 수정 및 기본 폰트제거

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Payload from '@/payload';
+import Link from 'next/link';
 
 const Footer = () => {
   const { githubNick } = Payload.info;
@@ -7,14 +8,14 @@ const Footer = () => {
     <footer className="w-full h-10 flex items-center justify-center text-[16px] font-bold bg-gray-600">
       <span className="text-blue-400">v.1.0.0</span>
       <span className="mx-2 text-white">|</span>
-      <a
+      <Link
         href={`https://github.com/${githubNick}/resume-nextjs`}
         target="_blank"
         rel="noreferrer"
         className="text-white"
       >
         GitHub
-      </a>
+      </Link>
     </footer>
   );
 };
